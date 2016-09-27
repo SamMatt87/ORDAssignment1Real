81a30728ee284cbeff12bb541cb116a64398b7c0
norgefier <norgefier@gmail.com>
2016-09-27 16:56:50 +1000

    Updated inserts
    Added disc 2 tracks
    Added insert statements for artists and roles

--- Inserts.sql
+++ Inserts.sql
@@ -1,4 +1,4 @@
-INSERT INTO ALBUM VALUES(CD_TYPE(1,'Tchaikovsky: Swan Lake (Complete Ballet)(DISC1)', 180, 'Classical', TO_DATE('01/06/2004', 'DD/MM/RRRR'),
+INSERT INTO ALBUM VALUES(CD_TYPE('ALB1','Tchaikovsky: Swan Lake (Complete Ballet)', 180, 'Classical', TO_DATE('01/06/2004', 'DD/MM/YYYY'),
 TRACKS(TRACK_TYPE(1,'Introduction: Moderato Assai - Allegro Ma Non Troppo',180,0.99),
 TRACK_TYPE(2,'No.1 - Scene: Allegro Giusto',180,0.99),
 TRACK_TYPE(3,'No.2 - Valse: Tempo Di Valse',180,0.99),
@@ -24,9 +24,46 @@ TRACK_TYPE(22,'I: Tempo Di Valse',180,0.99),
 TRACK_TYPE(23,'II: Moderato Assai - Molto Piu Mosso',180,0.99),
 TRACK_TYPE(24,'III: Danses Des Cygnes: Tempo Di Valse',180,0.99),
 TRACK_TYPE(25,'IV: Allegro Moderato',180,0.99),
-TRACK_TYPE(26,'V: Pas D Action (Odette Et Le Prince): Andante - Andante Non Troppo - Allegro - Norman Carol',180,0.99)),
+TRACK_TYPE(26,'V: Pas D Action (Odette Et Le Prince): Andante - Andante Non Troppo - Allegro - Norman Carol',180,0.99),
+TRACK_TYPE(27,'VI: Tempo Di Valse: Tout Le Monde Danse',180,0.99),
+TRACK_TYPE(28,'VII: Coda: Allegro Vivo',180,0.99),
+TRACK_TYPE(29,'No.14 - Scene: Moderato',180,0.99),
+TRACK_TYPE(30,'No.15 - Scene: Allegro Giusto',180,0.99),
+TRACK_TYPE(31,'No.16 - Danse Du Corps De Ballet Et Des Nains: Moderato Assai - Allegro Vivo',180,0.99),
+TRACK_TYPE(32,'No.17 - Scene: Sortie Des Invites Et Valse: Allegro - Tempo Di Valse',180,0.99),
+TRACK_TYPE(33,'No.18 - Scene: Allegro - Allegro Giusto',180,0.99),
+TRACK_TYPE(34,'I: Intrada: Moderato Assai',180,0.99),
+TRACK_TYPE(35,'II: Variation I: Allegro',180,0.99),
+TRACK_TYPE(36,'III: Variation II: Andante Con Moto',180,0.99),
+TRACK_TYPE(37,'IV: Variation III: Moderato',180,0.99),
+TRACK_TYPE(38,'V: Variation IV: Allegro',180,0.99),
+TRACK_TYPE(39,'VI: Variation V: Moderato - Allegro Semplice',180,0.99),
+TRACK_TYPE(40,'VII: Coda: Allegro Molto',180,0.99),
+TRACK_TYPE(41,'I: Moderato - Andante - Norman Carol',180,0.99),
+TRACK_TYPE(42,'II: Variation I: Allegro Moderato',180,0.99),
+TRACK_TYPE(43,'III: Variation II: Allegro',180,0.99),
+TRACK_TYPE(44,'III: Coda: Allegro Molto Vivace',180,0.99),
+TRACK_TYPE(45,'No.20 - Danse Hongroise - Czardas (Moderato Assai - Allegro Moderato - Vivace',180,0.99),
+TRACK_TYPE(46,'Appendix 2: Danse Russe: Moderato - Allegro Semplice - Allegro Vivo - Norman Carol',180,0.99),
+TRACK_TYPE(47,'No.21 - Danse Espagnole: Allegro Non Troppo, Tempo Di Bolero',180,0.99),
+TRACK_TYPE(48,'No.22 - Danse Napolitaine: Allegro Moderato - Andantino Quasi Moderato - Presto - Frank Kaderbarek',180,0.99),
+TRACK_TYPE(49,'No.23 - Mazurka: Solistes Et Corps De Ballet Tempo Di Mazurka',180,0.99),
+TRACK_TYPE(50,'No.24 - Scene: Allegro - Valse - Allegro Vivo',180,0.99),
+TRACK_TYPE(51,'No.25 - EntrActe: Moderato',180,0.99),
+TRACK_TYPE(52,'No.26 - Scene: Allegro Non Troppo',180,0.99),
+TRACK_TYPE(53,'No.27 - Danses Des Petits Cygnes: Moderato',180,0.99),
+TRACK_TYPE(54,'No.28 - Scene: Allegro Agitato - Molto Meno Mosso - Allegro Vivace',180,0.99),
+TRACK_TYPE(55,'No.29 - Scene Finale: Andante - Allegro Agitato - Alla Breve - Moderato',180,0.99)),
 REVIEWS_TABLE(
-REVIEW_TYPE(1,'Rudy Avila','Tchaikovskys Swan Lake remains a ballet favorite and a powerful star vehicle for the prima ballerina who must dance the roles of two characters- the "white swan" Odette and the black swan Odile, good and evil. Swan Lake has been recorded many times by celebrated conductors and their orchestras - Eugene Ormandy and the Philadelphia Orchestra (here its Wolfgang Sawallisch and the same orchestra from an earlier period) Canadian Charles Dutroit and the Montreal forces, Seiji Ozawa, Andre Previn and the London Symphony Orchestra, Richard Bonynge better known for his operatic vehicles with his wife Joan Sutherland, Antal Dorati and his orchestra, and even Herbert Von Karajan joined the band wagon of conducting great ballets- he conducted the complete Giselle and a concert suite of Tchaikovskys Swan Lake, Sleeping Beauty and Nutcracker. Wolfgang Sawallisch was a brilliant conductor, and unfortunately like so many conductors in Karajans time, he was underappreciated. This Swan Lake is beautifully restored and sounds fresh, powerful and lyrical. The music for Swan Lake has symphonic qualities, with a contrast of serene and jovial dances and melancholy and even fatalistic themes. The Dance of the Little Swans is bouncy and cheery, the violin solos for Odettes solo dances are almost mournful (shes mourning her swan condition/curse) and the Swan Theme is powerful and stormy, borrowed from the last notes to a Wagners Lohengrin prelude. For these reasons critics did not initially warm up to the "heavy" music of Swan Lake. But eventually it became the most beautiful of the "ballet-blancs" ever made. Other "white tutu" ballets include Giselle (Act 2 the Willis) and La Bayadere (Act 2 The Land of Shades). The music is to die for in this recording and I highly recommend it.',5.0,TO_DATE('06/07/2005', 'DD/MM/RRRR')),
-REVIEW_TYPE(2,'Mateo','Herbert Von Karajan comes to mind. Slow. No sense of the arc of the entire dance, but rather a series of orchestral pieces that are played mostly mezzo forte, with limited drama and zero spontaneity. In sum, the performance fails to realize the composers intentions. I purchased the recording based on the reviews here, but will be giving it away as a gift. I may try again with Bonynges recording, which is said to be more balletic, or continue to look into the more obscure Russian recordings.', 3.0, TO_DATE('01/07/2010', 'DD/MM/RRRR')),
-REVIEW_TYPE(3, 'S. Vinokurov', 'Love the CD (it has two rare favs of mine... the faster Black Swan Variation and the extended Russian Dance) however, I was shocked not to find Black Swan Pas de Deux. How can you have a COMPLETE Swan Lake recording and not include that?!?!?!',4.0, TO_DATE('31/07/2006', 'DD/MM/RRRR'))
+REVIEW_TYPE(1,'Rudy Avila','Tchaikovskys Swan Lake remains a ballet favorite and a powerful star vehicle for the prima ballerina who must dance the roles of two characters- the "white swan" Odette and the black swan Odile, good and evil. Swan Lake has been recorded many times by celebrated conductors and their orchestras - Eugene Ormandy and the Philadelphia Orchestra (here its Wolfgang Sawallisch and the same orchestra from an earlier period) Canadian Charles Dutroit and the Montreal forces, Seiji Ozawa, Andre Previn and the London Symphony Orchestra, Richard Bonynge better known for his operatic vehicles with his wife Joan Sutherland, Antal Dorati and his orchestra, and even Herbert Von Karajan joined the band wagon of conducting great ballets- he conducted the complete Giselle and a concert suite of Tchaikovskys Swan Lake, Sleeping Beauty and Nutcracker. Wolfgang Sawallisch was a brilliant conductor, and unfortunately like so many conductors in Karajans time, he was underappreciated. This Swan Lake is beautifully restored and sounds fresh, powerful and lyrical. The music for Swan Lake has symphonic qualities, with a contrast of serene and jovial dances and melancholy and even fatalistic themes. The Dance of the Little Swans is bouncy and cheery, the violin solos for Odettes solo dances are almost mournful (shes mourning her swan condition/curse) and the Swan Theme is powerful and stormy, borrowed from the last notes to a Wagners Lohengrin prelude. For these reasons critics did not initially warm up to the "heavy" music of Swan Lake. But eventually it became the most beautiful of the "ballet-blancs" ever made. Other "white tutu" ballets include Giselle (Act 2 the Willis) and La Bayadere (Act 2 The Land of Shades). The music is to die for in this recording and I highly recommend it.',5.0,TO_DATE('06/07/2005', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,'Mateo','Herbert Von Karajan comes to mind. Slow. No sense of the arc of the entire dance, but rather a series of orchestral pieces that are played mostly mezzo forte, with limited drama and zero spontaneity. In sum, the performance fails to realize the composers intentions. I purchased the recording based on the reviews here, but will be giving it away as a gift. I may try again with Bonynges recording, which is said to be more balletic, or continue to look into the more obscure Russian recordings.', 3.0, TO_DATE('01/07/2010', 'DD/MM/YYYY')),
+REVIEW_TYPE(3, 'S. Vinokurov', 'Love the CD (it has two rare favs of mine... the faster Black Swan Variation and the extended Russian Dance) however, I was shocked not to find Black Swan Pas de Deux. How can you have a COMPLETE Swan Lake recording and not include that?!?!?!',4.0, TO_DATE('31/07/2006', 'DD/MM/YYYY'))
 ), 1, 11.99));
+
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART1','Piotr Ilyitch Tchaikovsky'));
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART2','Wolfgang Sawallisch'));
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART3','Philadelphia Orchestra'));
+
+INSERT INTO ROLE VALUES(ROLE_TYPE('ART1', 'ALB1','Composer'));
+INSERT INTO ROLE VALUES(ROLE_TYPE('ART2', 'ALB1','Conductor'));
+INSERT INTO ROLE VALUES(ROLE_TYPE('ART3', 'ALB1','Orchestra'));
