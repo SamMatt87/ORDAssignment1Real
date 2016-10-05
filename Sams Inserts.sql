b10d91505fc6d9210eef921ec07a1413807059fd
Sam Matthews <sam.matthews.77@gmail.com>
2016-10-06 09:32:06 +1100

    Inserts so far.
    Issue where one of the attributes is always “larger than specified
    precision allowed for this column" don’t know which one.

--- Sams Inserts.sql
+++ Sams Inserts.sql
@@ -1,9 +1,68 @@
+INSERT INTO CD VALUES (CD_TYPE('ALB996', 'The Freewheelin Bob Dylan',49,'Rock', TO_DATE('12/8/1988', 'DD/MM/YYYY'),
+TRACKS(TRACK_TYPE(1, 'Blowin in the Wind', 165, 0.99, 50),
+TRACK_TYPE(2, 'Girl from the North Country', 199, 0.99, 50),
+TRACK_TYPE(3, 'Masters of War', 271, 0.99, 50),
+TRACK_TYPE(4, 'Down the Highway', 204, 0.99, 50),
+TRACK_TYPE(5, 'Bob Dylans Blues', 140, 0.99, 50),
+TRACK_TYPE(6, 'A Hard Rains A-Gonna Fall', 412, 0.99, 50),
+TRACK_TYPE(7, 'Dont Think Twice, Its Alright', 217, 0.99, 50),
+TRACK_TYPE(8, 'Bob Dylans Dream', 300, 0.99, 50),
+TRACK_TYPE(9, 'Oxford Town', 107, 0.99, 50),
+TRACK_TYPE(10, 'Talkin World War III Blues', 385, 0.99, 50),
+TRACK_TYPE(11, 'Corrina, Corrina', 161, 0.99, 50),
+TRACK_TYPE(12, 'Honey, Just Allow Me One More Chance', 118, 0.99, 50),
+TRACK_TYPE(13, 'I Shall Be Free', 840, 1.32, 50)),
+REVIEWS_TABLE(REVIEW_TYPE(1,'Josephine', 'Dated, not classic.', 1.0, TO_DATE('22/10/2014', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,'Donald Meek', 'I have many CDs in my juke boxes yet I get SACDs for the 5.1 surround mix this one is plain stereo so its just ok love bob dylan yet wanted him in surround.', 3.0, TO_DATE('18/10/2013','DD/MM/YYYY')),
+REVIEW_TYPE(3, Null, 'One of the clearest records Ive ever listened to. Very high quality!', 5.0, TO_DATE('24/08/2016', 'DD/MM/YYYY'))),1,10.18));
+/
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART996','Bob Dylan'));
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Flugelhorn/Trumpet'
+	FROM ARTIST ART, ALBUM ALB
+	WHERE ARTIST_ID = 'ART996' AND ALB.ALBUM_ID = 'ALB996';
+/
+INSERT INTO CD VALUES (CD_TYPE('ALB997', 'Weld',121,'Pop', TO_DATE('22/10/1991', 'DD/MM/YYYY'),
+TRACKS(TRACK_TYPE(1, 'Hey Hey, My My (Into The Black) (1991 Live)', 342, 1.32, 50),
+TRACK_TYPE(2, 'Crime In the City (1991 Live)', 392, 1.32, 50),
+TRACK_TYPE(3, 'Blowin In the Wind (1991 Live)', 409, 1.32, 50),
+TRACK_TYPE(4, 'Welfare Mothers (1991 Live)', 424, 1.32, 50),
+TRACK_TYPE(5, 'Love to Burn (1991 Live)', 601, 1.32, 50),
+TRACK_TYPE(6, 'Cinamon Girl (1991 Live)', 285, 1.32, 50),
+TRACK_TYPE(7, 'Mansion on the Hill (1991 Live)', 374, 1.32, 50),
+TRACK_TYPE(8, 'Fuckin Up (1991 Live)', 429, 1.32, 50),
+TRACK_TYPE(9, 'Cortez the Killer (1991 Live)', 586, 1.32, 50),
+TRACK_TYPE(10, 'Powderfinger (1991 Live)', 358, 1.32, 50),
+TRACK_TYPE(11, 'Love and Only Love (1991 Live)', 557, 1.32, 50),
+TRACK_TYPE(12, 'Rockin in the Free World (1991 Live Album Version)', 562, 1.32, 50),
+TRACK_TYPE(13, 'Like a Hurricane (1991 Live)', 840, 1.32, 50),
+TRACK_TYPE(14, 'Farmer John (Into The Black) (1991 Live)', 300, 1.32, 50),
+TRACK_TYPE(15, 'Tonights the Night (1991 Live)', 525, 1.32, 50),
+TRACK_TYPE(16, 'Roll Another Number (1991 Live)', 319, 1.32, 50)),
+REVIEWS_TABLE(REVIEW_TYPE(1,'David Marshall', 'Any live album will be enjoyed by all', 5.0, TO_DATE('15/06/2016', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,'Paul Hackbath', 'Very good.', 3.0, TO_DATE('20/05/2016','DD/MM/YYYY')),
+REVIEW_TYPE(3, Null, 'How could you go wrong when its Neil?', 4.0, TO_DATE('24/08/2016', 'DD/MM/YYYY'))),1,10.18));
+/
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART998','Neil Young'));
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART997','Crazy Horse'));
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Singer'
+	FROM ARTIST ART, ALBUM ALB
+	WHERE ARTIST_ID = 'ART998' AND ALB.ALBUM_ID = 'ALB997';
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Band'
+	FROM ARTIST ART, ALBUM ALB
+	WHERE ARTIST_ID = 'ART997' AND ALB.ALBUM_ID = 'ALB997';
+/
 INSERT INTO ALBUM VALUES(CD_TYPE('ALB999','Sketches of Spain', 45, 'Jazz', TO_DATE('01/01/1960', 'DD/MM/YYYY'),
-TRACKS(TRACK_TYPE(1,'Concerto de Arunjez: Adagio',981,0.99),
-TRACK_TYPE(2,'Will O the wisp',229,0.99),
-TRACK_TYPE(3,'The Pan Piper',237,0.99),
-TRACK_TYPE(4,'Saeta',335,0.99),
-TRACK_TYPE(5,'Solea',938,0.99)),
+TRACKS(TRACK_TYPE(1,'Concerto de Arunjez: Adagio',981,0.99,10),
+TRACK_TYPE(2,'Will O the wisp',229,0.99,50),
+TRACK_TYPE(3,'The Pan Piper',237,0.99,40),
+TRACK_TYPE(4,'Saeta',335,0.99,30),
+TRACK_TYPE(5,'Solea',938,0.99,10)),
 REVIEWS_TABLE(
 REVIEW_TYPE(1,'Betty Bo','Recording quality is good for such an old recording',3.0,TO_DATE('23/09/2016', 'DD/MM/YYYY')),
 REVIEW_TYPE(2,NULL,'Ive tried....really tried with this one , but it just doesnt do anything for me.But Ill try again ....maybe eventually it will click.', 3.0, TO_DATE('30/09/2010', 'DD/MM/YYYY')),
@@ -18,13 +77,13 @@ INSERT INTO ROLE
 	WHERE ARTIST_ID = 'ART999' AND ALB.ALBUM_ID = 'ALB999';
 /
 INSERT INTO ALBUM VALUES(CD_TYPE('ALB998','Bitches Brew', 105, 'Jazz', TO_DATE('08/06/1999', 'DD/MM/YYYY'),
-TRACKS(TRACK_TYPE(1,'Pharohs dance',1204,0.99),
-TRACK_TYPE(2,'Bitches Brew',1619,0.99),
-TRACK_TYPE(3,'Spanish Key',1052,0.99),
-TRACK_TYPE(4,'John Mclaughlin',262,0.99),
-TRACK_TYPE(5,'Miles Runs the Voodoo Down',841,0.99),
-TRACK_TYPE(6,'Sanctuary',656,0.99),
-TRACK_TYPE(5,'Feio',709,0.99)),
+TRACKS(TRACK_TYPE(1,'Pharohs dance',1204,0.99,10),
+TRACK_TYPE(2,'Bitches Brew',1619,0.99,10),
+TRACK_TYPE(3,'Spanish Key',1052,0.99,40),
+TRACK_TYPE(4,'John Mclaughlin',262,0.99,30),
+TRACK_TYPE(5,'Miles Runs the Voodoo Down',841,0.99,50),
+TRACK_TYPE(6,'Sanctuary',656,0.99,10),
+TRACK_TYPE(5,'Feio',709,0.99,10)),
 REVIEWS_TABLE(
 REVIEW_TYPE(1,NULL,'Im getting broken in. Have listened to it 3 times and am still trying to like it. I like some other Miles Davis albums waaaaay better!
 My top 3:
@@ -33,7 +92,7 @@ My top 3:
 3) In a Silent Way',3.0, TO_DATE('23/09/2016', 'DD/MM/YYYY')),
 REVIEW_TYPE(2,NULL,'Been waiting a long time to hear this again. Miles is brilliant!', 5.0, TO_DATE('21/09/2016', 'DD/MM/YYYY')),
 REVIEW_TYPE(3, 'Rita Sweeney', 'Love it!',5.0, TO_DATE('29/6/2016', 'DD/MM/YYYY'))
-),1,14.87);
+),1,14.87));
 /
 INSERT INTO ALSO_BOUGHT
 SELECT REF(ALB1), REF(ALB2)
