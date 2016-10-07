3bc2a0c64a2ce3e866642342fc95fd74387bffb2
Sam Matthews <sam.matthews.77@gmail.com>
2016-10-07 17:20:44 +1100

    More Inserts
    Joe Cocker

--- Sams Inserts.sql
+++ Sams Inserts.sql
@@ -1,4 +1,75 @@
+INSERT INTO CD VALUES (CD_TYPE('ALB986', 'I Can Stand A Little Rain',46,'Rock', TO_DATE('05/10/1999', 'DD/MM/YYYY'),
+TRACKS(TRACK_TYPE(1, 'Put Out The Light', 250, 1.29, 50),
+TRACK_TYPE(2, 'I Can Stand A Little Rain', 212, 1.29, 50),
+TRACK_TYPE(3, 'I Get Mad', 217, 1.29, 50),
+TRACK_TYPE(4, 'Sing Me A Song', 144, 1.29, 50),
+TRACK_TYPE(5, 'The Moon Is A Harsh Mistress', 210, 1.29, 50),
+TRACK_TYPE(6, 'Dont Forget Me', 198, 1.29, 50),
+TRACK_TYPE(7, 'You Are So Beautiful', 159, 1.29, 50),
+TRACK_TYPE(8, 'Its A Sin When You Love Somebody', 227, 1.29, 50),
+TRACK_TYPE(9, 'Performance', 277, 1.29, 50),
+TRACK_TYPE(10, 'Guilty', 166, 1.29, 50)),
+REVIEWS_TABLE(REVIEW_TYPE(1,Null, 'Not as good as some...', 2.0, TO_DATE('02/08/2014', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,Null, 'Went from 8 track to cassette to finally a CD of this album, love every song!', 5.0, TO_DATE('20/06/2016','DD/MM/YYYY')),
+REVIEW_TYPE(3, null, 'Not well recorded and song selection is poor does not bring to life his full voice command Winterland San Francisco 1968 was the best.', 1.0, TO_DATE('23/08/2013', 'DD/MM/YYYY'))),1,3.35));
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Singer'
+	FROM ARTIST ART, CD ALB
+	WHERE ART.ARTIST_ID = 'ART989' AND ALB.ALBUM_ID = 'ALB986';
+/
+INSERT INTO CD VALUES (CD_TYPE('ALB987', 'Joe Cocker!',46,'Rock', TO_DATE('05/10/1999', 'DD/MM/YYYY'),
+TRACKS(TRACK_TYPE(1, 'Dear Landlord (Album Version)', 205, 1.29, 50),
+TRACK_TYPE(2, 'Bird On A Wire (Album Version)', 268, 1.29, 50),
+TRACK_TYPE(3, 'Lawdy Miss Clawdy (Album Version)', 133, 1.29, 50),
+TRACK_TYPE(4, 'She Came In Through The Bathroom Window (Album Version)', 156, 1.29, 50),
+TRACK_TYPE(5, 'Hitchcock Railway (Album Version)', 277, 1.29, 50),
+TRACK_TYPE(6, 'Thats Your Business (Album Version)', 176, 1.29, 50),
+TRACK_TYPE(7, 'Something (Album Version)', 211, 1.29, 50),
+TRACK_TYPE(8, 'Delta Lady (Album Version)', 170, 1.29, 50),
+TRACK_TYPE(9, 'Hello, Little Friend (Album Version)', 312, 1.29, 50),
+TRACK_TYPE(10, 'Darling Be Home Soon (Album Version)', 485, 1.29, 50),
+TRACK_TYPE(11, 'Shes Good To Me (Single Version)', 180, 1.29, 50),
+TRACK_TYPE(12, 'Let It Be (Original Take)', 300, 1.29, 50)),
+REVIEWS_TABLE(REVIEW_TYPE(1,Null, 'Not 5 stars .... mumbled lyrics , bad production, think its over rated from covers of beatles and Leon Russell connection.', 2.0, TO_DATE('23/05/2016', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,Null, 'The original and the greatest. Joe, I really miss you', 5.0, TO_DATE('16/05/2015','DD/MM/YYYY')),
+REVIEW_TYPE(3, null, 'Great.', 5.0, TO_DATE('21/02/2015', 'DD/MM/YYYY'))),1,3.35));
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Singer'
+	FROM ARTIST ART, CD ALB
+	WHERE ART.ARTIST_ID = 'ART989' AND ALB.ALBUM_ID = 'ALB987';
+/
+INSERT INTO CD VALUES (CD_TYPE('ALB988', 'With A Little Help From My Friends',46,'Rock', TO_DATE('05/10/1999', 'DD/MM/YYYY'),
+TRACKS(TRACK_TYPE(1, 'Feelin Alright', 250, 1.29, 50),
+TRACK_TYPE(2, 'Bye Bye Blackbird', 207, 1.29, 50),
+TRACK_TYPE(3, 'Change In Louise', 203, 1.29, 50),
+TRACK_TYPE(4, 'Marjorine', 158, 1.29, 50),
+TRACK_TYPE(5, 'Just Like A Woman', 317, 1.29, 50),
+TRACK_TYPE(6, 'Do I Still Figure In Your Life?', 239, 1.29, 50),
+TRACK_TYPE(7, 'Sandpaper Cadillac', 196, 0.99, 50),
+TRACK_TYPE(8, 'Dont Let Me Be Misunderstood', 281, 1.29, 50),
+TRACK_TYPE(9, 'With A Little Help From My Friends', 312, 1.29, 50),
+TRACK_TYPE(10, 'I Shall Be Released', 276, 1.29, 50),
+TRACK_TYPE(11, 'The New Age of Lilly', 135, 1.29, 50),
+TRACK_TYPE(12, 'Somethings Coming On', 135, 1.29, 50)),
+REVIEWS_TABLE(REVIEW_TYPE(1,Null, 'Masterful album with more soul than you can shake a stick to. Cockers version of Bye Bye Blackbirdis worth the price alone.
+When Cocker does covers of the Beatles or Dylan, he does sonething that is difficult to do--he adds new elements and takes em to a new level.
+This record is a must for the audiophile who loves 60s R and B or just good ole rock n roll.', 5.0, TO_DATE('20/05/2000', 'DD/MM/YYYY')),
+REVIEW_TYPE(2,Null, 'This is Joes first album and it is his best IMHO. His following release, Joe Cocker, is also highly praised but I think falls a distant second to this one. Alot of soul and great musicians to back him up, including Jimmy Page. This one is great.', 5.0, TO_DATE('12/11/2004','DD/MM/YYYY')),
+REVIEW_TYPE(3, null, 'Joe Cocker and a stellar case of rocks Whos Who from the late `60s overcame a bit of turbulence in the studio to create one of the defining works of that time, a great introduction to Mr. Cocker, and a great reminder of just how much talent and musicianship existed then.
 
+Listening to these songs now, its interesting to hear Jimmy Page underplaying his parts and to hear how Albert Lee was already the consummate guitar session man. Of course, this album is all about vocals, and Mr. Cocker let the world know he was ready to be reckoned with on this debut. The fine backup singers really help frame his sometimes out-of-bounds singing, but this set still swings and sound remarkably fresh considering it is 40-plus years old now.
+
+The sound is a bit cleaner on this remaster, and the bonus tracks, while worth a listen, neither add to or detract from the recordings status.', 5.0, TO_DATE('19/03/2010', 'DD/MM/YYYY'))),1,4.99));
+/
+INSERT INTO ARTIST VALUES(ARTIST_TYPE('ART989','Joe Cocker'));
+/
+INSERT INTO ROLE
+	SELECT REF(ART), REF(ALB), 'Singer'
+	FROM ARTIST ART, CD ALB
+	WHERE ART.ARTIST_ID = 'ART989' AND ALB.ALBUM_ID = 'ALB988';
+/
 INSERT INTO CD VALUES (CD_TYPE('ALB989', 'The Magnificent Moodies',74,'Rock', TO_DATE('15/12/2014', 'DD/MM/YYYY'),
 TRACKS(TRACK_TYPE(1, 'Ill Go Crazy', 131, 0.99, 50),
 TRACK_TYPE(2, 'Something You Got', 172, 0.99, 50),
